9c07a5174170a5a8f62f718a47b0e9f351a2ab57
Mahami mathias <mathiasmahami@gmail.com>
2025-02-17 10:31:49 +0000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull mat94/simple-python-flask-app
+docker pull mat94/simple-python-flask-app 
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 mat94/simple-python-flask-app
